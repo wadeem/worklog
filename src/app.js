bb58725b5e519bb1441f8e9617416ea21beac708
max3d <vadim@unikivi.ee>
2019-08-18 11:24:26 +0300

    justifyContent:center

--- src/app.js
+++ src/app.js
@@ -6,7 +6,7 @@ import Main from './components/main.js';
 
 const App = () => {
     const {container, header, main, footer} = styles;
-    
+
     return <View style={container}>
         <Header style={header}/>
         <Main style={main}/>
@@ -21,7 +21,7 @@ const styles = StyleSheet.create({
             flex: 6,
         },
         header: {flex: 1, backgroundColor: 'blue'},
-        main: {flex: 4, backgroundColor: 'white'},
+        main: {flex: 4, backgroundColor: 'white',justifyContent:'center'},
         footer: {flex: 1, backgroundColor: 'yellow'},
     },
 );
